a94a05714cae6bed39f1b48086c0dd77e20890be
Taylor Lightbourne <79942688+taylorlightbourne@users.noreply.github.com>
2024-02-23 10:06:06 -0500

    changed class name

--- Snake-3D/Assets/Control.cs
+++ Snake-3D/Assets/Control.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class NewBehaviourScript : MonoBehaviour
+public class Control : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
