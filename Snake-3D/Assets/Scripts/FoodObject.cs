35508d0d912c21a4f393eef14bab1ec4ad9b9efd
Ryan Smith <67984581+ryan-matthew-smith@users.noreply.github.com>
2024-03-05 13:10:41 -0500

    Environment Art (#6)
    * Initial Art
    
    * Cage Materials Update
    
    * Bubbles & Cage Update
    
    * Skybox Updates
    
    * Environment Prefabs
    
    * Revert "Merge branch 'main' into dev-Art"
    
    This reverts commit 935c2c894703fd5cc06b248089efc935563db3bf, reversing
    changes made to daf4fd1e908423d1350a17a8d45bfd5354454eff.
    
    * Environment integration with SnakeLevel Scene

--- Snake-3D/Assets/Scripts/FoodObject.cs
+++ Snake-3D/Assets/Scripts/FoodObject.cs
@@ -1,52 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
-using Unity.VisualScripting;
 using UnityEngine;
 
 public class FoodObject : MonoBehaviour
 {
-
-    public BoxCollider gridArea;
-
-
+    // Start is called before the first frame update
     void Start()
-
-
     {
-
-        RandomizePositiion();
-
-    }
-
-    private void RandomizePositiion()
-
-    {
-
-        Bounds bounds = this.gridArea.bounds;
-
-        float x = Random.Range(bounds.min.x, bounds.max.x);
-
-        float y = Random.Range(bounds.min.y, bounds.max.y);
-
-        float z = Random.Range(bounds.min.z, bounds.max.z);
-
-        this.transform.position = new Vector3(x, y, z);
-
+       
     }
 
     // Update is called once per frame
-
-    private void OnTriggerEnter(Collider other)
-
+    void Update()
     {
-
-        if (other.tag == "Player")
-
-        {
-
-            RandomizePositiion();
-
-        }
-
+        
     }
+
 }
